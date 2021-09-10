5b32af0af6034e37b0cad0be2eb3da10d6b85046
Ghislain <ghislain.jambert@gmail.com>
2021-09-10 07:55:37 +0200

    insertion fonction scrolling de page

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
-//APPELS DES ÉLÉMENTS DU  DOM
+//--------------------------------------------------------------------------------------//
+//                             APPELS DES ÉLÉMENTS DU  DOM                              //
+//--------------------------------------------------------------------------------------//
+
 const nav = document.getElementById('nav');
 const navBar = document.getElementsByClassName('nav-bar'); 
 const selectionPhotographes = document.getElementById('selection-photographes');
@@ -13,16 +16,25 @@ const cadreTagsPhotographe = document.getElementsByClassName('cadre-tags-photogr
 const baliseClassTag = document.getElementsByClassName('tag');
 const baliseIdTag = document.getElementById("tag");
 
-//APPEL DU JSON
+let tagsPhotoProfil = []
+let tagsNavBar = [];
+
+//--------------------------------------------------------------------------------------//
+//                                    APPEL DU JSON                                     //
+//--------------------------------------------------------------------------------------//
 
 fetch('assets/json/listeObjets.json')
 .then(response => response.json())
 .then(response => {
     console.log(response);
     profilsPhotographes(response.photographers)
+    allPhotographers = response.photographers;
 });
 
-//FONCTIONS D APPEL
+//--------------------------------------------------------------------------------------//
+//                                  FONCTIONS D APPEL                                   //
+//--------------------------------------------------------------------------------------//
+
 
 function profilsPhotographes(photographers) {
 //      Création variable type array vide pour regrouper les tags des photographes      //
@@ -30,7 +42,7 @@ function profilsPhotographes(photographers) {
 //      Sortir les photographes individuellement et renvoi la fonction d'insertion      //
     for (let i = 0; i < photographers.length; i++) {
         const photographe = photographers[i];
-        insertionArticle = insertionArticlePhotographe(photographe);
+        insertionArticlePhotographe(photographe);
         for (let j = 0; j < photographe.tags.length; j++) {
             const tag = photographe.tags[j];
             allTags.push(tag)
@@ -41,10 +53,13 @@ function profilsPhotographes(photographers) {
     console.log(allTags);
 //      Renvoi vers la fonction d'insertion des tags dans la barre de naviguation       //
     navBarTags(allTags);
-    selecTag();
+    TagsSelection(tagsPhotoProfil, allTags);
 }
 
-//FONCTIONS DE TRAITEMENT
+//--------------------------------------------------------------------------------------//
+//                               FONCTIONS DE TRAITEMENT                                //
+//--------------------------------------------------------------------------------------//
+
 
 function navBarTags(tags) {
 //                      Création balise div pour tags de la navBar                      //
@@ -53,8 +68,9 @@ function navBarTags(tags) {
         insertionNavBar.classList.add("nav-bar");
         for (let i = 0; i < tags.length; i++) {
         let tag = tags[i];
-        insertionNavBar.innerHTML += `<a id="tag" class="tag ${tag}">${tag}</a>`;
+        insertionNavBar.innerHTML += `<a id="tag" class="tag ${tag} nav-tag">${tag}</a>`;
     }
+    tagsNavBar.push(insertionNavBar.childNodes)
 //                                Insertion de la balise                                //
     nav.appendChild(insertionNavBar);
 };
@@ -94,6 +110,7 @@ function insertionArticlePhotographe(photographe) {
         const tag = photographe.tags[j];
         insertionTags.innerHTML += `<a id="tag" class="tag ${tag} tag-photographe">${tag}</a>`;
     }
+    tagsPhotoProfil.push(insertionTags.childNodes)
 
 //                          Insertion des différentes balises                           //
     articleProfil.appendChild(insertionFigure);
@@ -103,30 +120,110 @@ function insertionArticlePhotographe(photographe) {
     articleProfil.appendChild(insertionTags);
 };
 
-//FONCTION D'ÉCOUTE DES TAGS
+//--------------------------------------------------------------------------------------//
+//                              FONCTION D'ÉCOUTE DES TAGS                              //
+//--------------------------------------------------------------------------------------//
 
-function selecTag() {
-//Lorsque je clique sur un tag ajoute la classe .focus
+
+function TagsSelection(tagsPhotoProfil, allTags) {
+    console.log(tagsPhotoProfil)
     for (let i = 0; i < baliseClassTag.length; i++) {
         const lien = baliseClassTag[i];
+        //Lorsque je clique sur un tag ajoute la classe .focus
         lien.addEventListener("click", function(e) {
             e.preventDefault;
             this.classList.toggle("focus")
             let tagsFocus = document.querySelectorAll('a.tag.focus');
-            let tagsPhotographe = document.querySelectorAll('a.tag-photographe');
-            console.log(tagsFocus)
-//Si .focus afficher que les articles du photographes en fonction du tag focus
-        for (let j = 0; j < tagsPhotographe.length; j++) {
-            const tagPhoto = tagsPhotographe[j];
-            photographeTag(tagsFocus, tagPhoto)
+            // Mettre à jour les articles des photographes selon les tags selectionnés
+            // Pour chaque photographes boucle for photographe.tags
+            for (let j = 0; j < tagsPhotoProfil.length; j++) {
+                const tagPhoto = tagsPhotoProfil[j];
+                searchTags(tagsFocus, tagPhoto, allTags)
             }
+                // comparer les tags focus avec les tags du photographe
+                    // si true 
+                        // afficher le photographe dans le html
+                    // sinon 
+                        // le cacher
+            
+            //Récupérer les tags sélectionnés et les comparer avec les tags de chaques photographes
+            //Si le photographes ne contients pas les tags sélectionnés alors caché le photographe
+            //Sinon l'affiché
         }
     )}
 };
 
-function photographeTag(tagsFocus, tagPhoto) {
-    if(tagPhoto.innerHTML != tagsFocus.innerHTML) {
-        let article = tagPhoto.parentNode.parentNode
-        article.classList.add('invisible');
+function searchTags(tagsFocus, tagsPhotographes, allTags) {
+    // comment on comparer les tags sel;ectionné avec les tags du photographe
+    // tags selectionné type : ?? tagsFocus: Array de nodes
+    // tags type : ?? array<string>
+    console.log(tagsFocus)
+    console.log(allTags)
+    for (let i = 0; i < tagsPhotographes.length; i++) {
+        const tagPhotographe = tagsPhotographes[i];
+        tagsPhotoProfil.includes(tagPhotographe)
+        console.log(tagPhotographe)
+        //let hasTagClass = 
+        if (tagPhotographe.innerHTML == tagsFocus.innerHTML) {
+            tagPhotographe.classList.add('focus')
+        }
     }
 }
+
+//--------------------------------------------------------------------------------------//
+//                             FONCTION D'ECOUTE DU SCROLL                              //
+//--------------------------------------------------------------------------------------//
+
+//                      fonction de la valeur du scroll de la page                      //
+let scrollYPage =  function() {
+    return window.scrollY
+}
+let retourIndex = document.querySelector('#retour-index')
+let rectangleIndex = retourIndex.getBoundingClientRect()
+// ---- Obtenir la largeur de l'élément retour-index par défaut --------------------------
+let widthIndex = rectangleIndex.width
+// ---- Obtenir la position de l'élément par rapport à la hauteur de la fenêtre ----------
+let topPage = rectangleIndex.top + scrollYPage()
+// ---- Creation d'une fausse balise pour ne pas casser la structure de la page ----------
+let fauxRectangle = document.createElement('div')
+fauxRectangle.style.width = widthIndex + 'px'
+fauxRectangle.style.height = rectangleIndex.height + 'px'
+//       Fonction onScroll qui permet l affichage fixe ou non de l'élément retour       //
+// ---- Lorsque l'on scroll --------------------------------------------------------------
+let onScroll = function () {
+    // ---- detection de la class 'fixe' -----------------------------------------------------
+    let hasScrollClass = retourIndex.classList.contains('fixe')
+    // ---- si le menu sort de l'écran et qu'il n'a pas la class 'fixe' ----------------------
+    if (scrollYPage() > topPage && !hasScrollClass) {
+        // ---- alors il devient fixe ------------------------------------------------------------
+        retourIndex.classList.remove('retour-index')
+        retourIndex.classList.add('fixe')
+        retourIndex.getElementsByClassName.width = widthIndex + 'px'
+        retourIndex.parentNode.insertBefore(fauxRectangle, retourIndex)
+        // ---- sinon il revient à sa place ------------------------------------------------------
+    } else if (scrollYPage() < topPage && hasScrollClass) {
+        retourIndex.classList.remove('fixe')
+        retourIndex.classList.add('retour-index')
+        retourIndex.parentNode.removeChild(fauxRectangle)
+    }
+}
+// Fonction onResize permet l'ajust du contenu en fonction de la largeur de la fenêtre  //
+let onResize = function() {
+    // ---- Deconstruction de ce qui à été fait dans la fct onScroll -------------------------
+    // ---- Attribution de la valeur par défaut de la largeur de l'élément -------------------
+    retourIndex.style.width = "182px"
+    retourIndex.classList.remove('fixe')
+    fauxRectangle.style.display = 'none'
+    // ---- recalcul de la position des éléments ---------------------------------------------
+    rectangleIndex = retourIndex.getBoundingClientRect()
+    topPage = rectangleIndex.top + scrollYPage()
+    // ---- Reattribution des valeurs du fauxRectangle ---------------------------------------
+    fauxRectangle.style.width = widthIndex + 'px'
+    fauxRectangle.style.height = rectangleIndex.height + 'px'
+    fauxRectangle.style.display = "block"
+    // ---- Retourne la fonction onScroll une fois le réajustement terminée ------------------
+    onScroll()
+}
+//          Ecoute du scroll de la fenêtre et du redimensionnement de la page           //
+window.addEventListener('scroll', onScroll)
+window.addEventListener('resize', onResize)
